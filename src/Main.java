2346a294b84749fe79e2f861fc96090e6f0acada
yakov23 <yakovoberderfer13@gmail.com>
2023-12-12 13:08:08 +0300

    git ignored

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 9000;
+        int bonus = 20;
+        System.out.println(price / bonus);
+        int mil = 450;
+        System.out.println(mil);
+
+    }
+}
